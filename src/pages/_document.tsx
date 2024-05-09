d49a4fdc6dee953fe8bf42dfc82b03f514cca21b
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-05-09 01:53:41 -0300

    chore: initial configs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <title>Overwatch API</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
