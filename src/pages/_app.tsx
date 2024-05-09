b43ddfd68e5492b13acabce5aec22a3c991ed6ed
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-05-09 03:19:39 -0300

    fix: add title to app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
+import Head from "next/head";
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={poppins.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <title>Home | Overwatch API</title>
+      </Head>
+      <main className={poppins.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
