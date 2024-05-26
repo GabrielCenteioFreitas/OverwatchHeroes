c95eae0f31036778adfe6ee4898ff7affcfdb00d
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-05-26 16:52:09 -0300

    perf: remove unused libs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 import { withNextVideo } from "next-video/process";
-const shouldAnalyzeBundles = process.env.ANALYZE === true;
 /** @type {import('next').NextConfig} */
 let nextConfig = {
   reactStrictMode: true,
 };
-
-if (shouldAnalyzeBundles) {
-  const withNextBundleAnalyzer =
-    require('next-bundle-analyzer')(/* options come there */);
-  nextConfig = withNextBundleAnalyzer(nextConfig);
-}
 
 export default withNextVideo(nextConfig);
